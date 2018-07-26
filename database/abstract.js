183e004d7301ca3fa4510664472b1133b7528fb8
jer <deathbyjer@gmail.com>
2018-07-26 17:26:35 -0400

    make the finds and saves parralel

--- database/abstract.js
+++ database/abstract.js
@@ -9,13 +9,25 @@ const Base = {
   
   async find(connection, map, id) { return {} },
   
-  // Todo: Parralelize this
   async findAll(connection, map, ids) { 
-    const out = {}
+    const promises = []
+    // We are going to not wait on them one at a time, but we'll wait on all of them together
     for (let id of ids)
-      out[id] = await this.find(connection, map, id)
+      promises.push(this.find(connection, map, id))
 
-    return out
+    // We make a new Promise that encapsulates a wait for all the results.
+    // When get those results, we will take the models that are returned and
+    // assign them to an "out" object by id
+    //
+    // Because Promise.all returns an array the side of the promises sent, and because it
+    // maintains the order of that array, we are able to do this
+    return new Promise((res, rej) => Promise.all(promises).then(models => {
+      const out = {}
+      for (let i in models)
+        out[ids[i]] = models[i]
+      
+      res(out)
+    }).catch(e => rej(e)))
   },
   
   async findIndex(connection, map, fields, values) {},
@@ -26,11 +38,11 @@ const Base = {
     if (ids.length != datas.length)
       return false
     
-    // TODO: Parralelize this
+    const promises = []
     for (let i = 0; i < ids.length; i++)
-      await this.save(connection, map, ids[i], datas[i])
+      promises.push(this.save(connection, map, ids[i], datas[i]))
     
-    return true
+    return Promise.all(promises)
   },
   
   async saveIndex(connection, map, fields, values, unique) {},
